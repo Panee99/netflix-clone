a0dfb41938cf5b5abca29dd0153456363f64290c
Nguyen Minh Hoang <hoangnm191999@gmail.com>
2024-01-22 00:22:46 +0700

    ...

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,6 +1,17 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GitHubProvider from "next-auth/providers/github";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
-    adapter: Adapters(prisma),
-    providers: []
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
 } satisfies NextAuthOptions;
